326baf1df37f2bd245e52f8671de2668def951b4
Robert1295 <benstine.robert2005@gmail.com>
2025-12-24 01:37:36 -0500

    added specific paths

--- backup.sh
+++ backup.sh
@@ -2,24 +2,31 @@
 
 # -----Configuration----- # 
 
+set -e									 # exit under ANY failure case #
+
 # -----Identify the Directories----- #
-MINECRAFT_DIR=
-BACKUP_DIR=
+MINECRAFT_DIR="/home/robertbenstine/minecraft/server"
+BACKUP_DIR="/home/robertbenstine/minecraft/world_backup"
 
 # -----Remote Console Config----- #
-RCON_HOST # HOST OF SERVER?
-RCON_PORT # PORT OF ACCESS
-RCON_PASS # PASSWORD
-RCON_TOOL # CLI - SSH for Minecraft Server
+RCON_HOST="100.122.225.11" # HOST OF SERVER?
+RCON_PORT="25575" # PORT OF ACCESS
+RCON_PASS="AndiShadow01!" # PASSWORD
+RCON_TOOL="/usr/local/bin/mcrcon" # CLI - SSH forwhich mcrcon Minecraft Server
 
 # -----BACKUP Logic----- # 
 
 
 TIMESTAMP=$ # Files Time of Backup #
-WORLD_FOLDER=$ # Accesses World Folder for Backup #
+
+
+WORLD_FOLDERS=(
+		"$~/minecraft/2-week phase (2 week phase)"      # Overworld #
+		"$~/minecraft/2-week phase (2 week phase)/DIM1" # End #
+		"$~/minecraft/2-week phase (2 week phase)/DIM-1" # Nether #
+		)
 
 # ----- Stop Auto-Save for World ----- #
-
 echo "Stopping Auto-Saves. . ."
 
 
